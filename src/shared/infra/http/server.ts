439d063daa727dde0d537e3f279abb711266b34e
Renan de Freitas <rnn.frts@gmail.com>
2020-06-05 22:44:58 -0300

    setting up heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('server started on port 3333');
 });
